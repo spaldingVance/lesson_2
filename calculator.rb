941560652728957c5b23ff3be873c7480569844d
Spalding Vance <sclarkvance@gmail.com>
2019-07-11 11:48:06 -0400

    edited calculator.rb with rubocop

--- calculator.rb
+++ calculator.rb
@@ -24,8 +24,6 @@ def operation_to_message(op)
   end
 end
 
-
-
 prompt("Welcome to Calculator! Enter your name:")
 
 name = ''
@@ -41,7 +39,7 @@ end
 
 prompt("Hi #{name}!")
 
-loop do #main loop
+loop do # main loop
   number1 = ''
   loop do
     prompt("What's the first number?")
@@ -55,7 +53,7 @@ loop do #main loop
   end
 
   number2 = ''
-  loop do 
+  loop do
     prompt("What's the second number?")
     number2 = Kernel.gets().chomp()
 
@@ -66,13 +64,10 @@ loop do #main loop
     end
   end
 
-
-  prompt("What operation would you like to perform? 1) add 2) subtract 3) multkiply 4) divide")
-  
   operator_prompt = <<-MSG
     What operation would you like to perform?
     1) add
-    2) subtract 
+    2) subtract
     3) multiply
     4) divide
   MSG
@@ -93,16 +88,15 @@ loop do #main loop
   prompt("#{operation_to_message(operator)} the two numbers...")
 
   result = case operator
-    when '1'
-      number1.to_i() + number2.to_i()
-    when '2'
-      number1.to_i() - number2.to_i()
-    when '3'
-      result = number1.to_i() * number2.to_i()
-    when '4'g
-      result = number1.to_f() / number2.to_f()
-  end
-
+           when '1'
+             number1.to_i() + number2.to_i()
+           when '2'
+             number1.to_i() - number2.to_i()
+           when '3'
+             number1.to_i() * number2.to_i()
+           when '4'
+             number1.to_f() / number2.to_f()
+           end
 
   prompt("The result is #{result}")
 
